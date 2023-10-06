8a16e1b3662f1bae9e1089bf9f16add8b6f71060
Chris <60541438+1-chris@users.noreply.github.com>
2023-10-07 00:46:12 +0100

    implement client sync on connection

--- Mateani/Server/Hubs/DrawHub.cs
+++ Mateani/Server/Hubs/DrawHub.cs
@@ -5,9 +5,24 @@ namespace Mateani.Server.Hubs;
 
 public class DrawHub : Hub
 {
+    private static List<DrawingCommand> _commands = new();
     public async Task SendCommand(DrawingCommand command)
     {
         // Console.WriteLine($"Server received command with color: {command.Color:X8}");
+        _commands.Add(command);
         await Clients.Others.SendAsync("ReceiveCommand", command);
+    }
+
+    public override async Task OnConnectedAsync()
+    {
+        if (_commands.Count > 0)
+        {
+            foreach (var command in _commands)
+            {
+                await Clients.Caller.SendAsync("ReceiveCommand",command);
+            }
+        }
+        
+        await base.OnConnectedAsync();
     }
 }
